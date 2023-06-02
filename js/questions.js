9d95596950bef2bd3ccaf2c0323f1e51dbb8b6f2
jrainey95 <jonathan.rainey5@gmail.com>
2023-06-01 21:51:27 -0700

    code

---
+++ js/questions.js
@@ -0,0 +1,42 @@
+var questions = [
+  {
+    title: "What is a function in Javascript?",
+    choices: [
+      "performs a task or calculates a value",
+      "an expression, rule, or law that defines a relationship between one variable",
+      "a standalone entity, with properties and type.",
+      "are containers for named values called properties",
+    ],
+    answer: "performs a task or calculates a value",
+  },
+  {
+    title: "What does the console.log() method do?",
+    choices: [
+      "logs a variable",
+      "runs a function",
+      "loops the code",
+      "logs a message to console",
+    ],
+    answer: "logs a message to console",
+  },
+  {
+    title: "What are arrays used for?",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
+    answer: "all of the above",
+  },
+  {
+    title: "What do if/else if/else statements do?",
+    choices: [
+      "executes a block of code is a specified contition is not true",
+      "executes a block of code if a specified condition is true",
+      "create a block of code",
+      "all the above",
+    ],
+    answer: "executes a block of code if a specified condition is true",
+  },
+];
